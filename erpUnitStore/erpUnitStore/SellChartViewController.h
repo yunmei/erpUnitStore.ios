fc0566de48d904737b0dca3d030c031f732cd7c9
wanghong <89240220@qq.com>
2013-03-26 03:04:21 +0000

    完成画折线图的功能

---
+++ erpUnitStore/erpUnitStore/SellChartViewController.h
@@ -0,0 +1,13 @@
+//
+//  SellChartViewController.h
+//  erpUnitStore
+//
+//  Created by ken on 13-3-24.
+//  Copyright (c) 2013年 maimaicha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SellChartViewController : UIViewController
+
+@end
---
+++ erpUnitStore/erpUnitStore/SellChartViewController.m
@@ -0,0 +1,38 @@
+//
+//  SellChartViewController.m
+//  erpUnitStore
+//
+//  Created by ken on 13-3-24.
+//  Copyright (c) 2013年 maimaicha. All rights reserved.
+//
+
+#import "SellChartViewController.h"
+
+@interface SellChartViewController ()
+
+@end
+
+@implementation SellChartViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
